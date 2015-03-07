aef2cf16cd0e01277e4eab0f90f368e13a5a7cf3
Link250 <dani.bg97@web.de>
2015-03-07 20:44:53 +0100

    Changed Bagsize back

--- src/ItemList/GiantItemBag.java
+++ src/ItemList/GiantItemBag.java
@@ -10,7 +10,7 @@ import Maps.Map;
 public class GiantItemBag extends ItemBag {
 
 	public GiantItemBag(){
-		super(40);
+		super(30);
 		ID = 403;
 		name = "GiantItemBag";
 		gfx = new SpriteSheet("/Items/NormalItemBag.png");
