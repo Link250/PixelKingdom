9ab6aa59f3a06f3732074bc10ae81cc0a8f9f792
ultrasn0w <r.barth@dshack.org>
2015-03-07 01:21:03 +0100

    Updating windows launch batch

--- windows_launcher/run.bat
+++ windows_launcher/run.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo ==========================================================
-echo                      PIXEL KINGDOM         [pub-Preview_4] 
+echo                      PIXEL KINGDOM         [pub-Preview_5] 
 echo ==========================================================
 
 echo Starte PixelKingdom...
-java -jar PixelKingdom.jar -XX:+UseFastAccessorMethods -XX:+BindGCTaskThreadsToCPUs -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -XX:+CICompilerCountPerCPU
+java -jar PixelKingdom.jar -XX:+UseFastAccessorMethods -XX:+BindGCTaskThreadsToCPUs -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy
 echo.
 echo.
 echo Errors bitte an Entwickler weitergeben mit Informationen zur Ursache.
