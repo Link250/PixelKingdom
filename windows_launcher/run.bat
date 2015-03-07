11b2394a7d478b2e902554c21a5a939b51e544fc
ultrasn0w <r.barth@dshack.org>
2015-03-07 21:31:26 +0100

    Update Console spam

--- windows_launcher/run.bat
+++ windows_launcher/run.bat
@@ -8,7 +8,7 @@ echo Starte PixelKingdom...
 java -jar PixelKingdom.jar -XX:+UseFastAccessorMethods -XX:+BindGCTaskThreadsToCPUs -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy
 echo.
 echo.
-echo Errors bitte an Entwickler weitergeben mit Informationen zur Ursache.
+echo Errors bitte an Entwickler weitergeben mit Informationen zur Ursache /Eine Issue auf unserem GitHub erstellen.
 echo Danke; das Entwicklerteam.
 echo.
 pause
