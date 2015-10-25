f87a9cf7e576c247b7c048e956b6f68f48009f57
Link250 <dani.bg97@web.de>
2015-10-25 10:39:33 +0100

    small changes on Fire

--- src/Pixels/Fire.java
+++ src/Pixels/Fire.java
@@ -16,14 +16,18 @@ public class Fire extends Material{
 	}
 	
 	public boolean tick(int numTick, Map map) {
-		map.setlighter(x, y, (byte) (Math.random()*32+16));
+		for (int X = -1; X <= 1; X++) {
+			for (int Y = -1; Y <= 1; Y++) {
+				if(map.getID(x+X, y+Y, 2)==1) {map.setID(x, y, l, 0);return true;}
+			}
+		}
+		ad = map.getAD(x, y, l);
+		byte n = ad.getbyte(0);
+		map.setlighter(x, y, (byte) (n/2+10));
 		if(Math.random()*500<1){
 			spread(map);
 		}
-		if(map.getID(x, y, 2)==1){map.setID(x, y, l, 0);return true;}
 		if(numTick%60==30){
-			ad = map.getAD(x, y, l);
-			byte n = ad.getbyte(0);
 			if(n>0){
 				ad.setbyte(0, (byte) (n-1));
 			}else{
@@ -34,16 +38,19 @@ public class Fire extends Material{
 	}
 	
 	public void render(Map map, Screen screen, int layer) {
-		screen.drawPixelScaled(x, y, 0xffff0000 | (((int)(Math.random()*256))<<8));
+		screen.drawPixelScaled(x, y, 0xffff0000 | (((byte)(map.getAD(x, y, l+1).getbyte(0)*2+Math.random()*55))<<8));
 	}
 	
 	public void spread(Map map){
 		byte burntime;
 		int Xt,Yt;
+		byte Xd,Yd;
+		if(Math.random()<0.5)Xd=1;else Xd=-1;
+		if(Math.random()<0.5)Yd=1;else Yd=-1;
 		for(int Y=-2; Y<=2; Y++){
 			for(int X=-2; X<=2; X++){
 				for(int L=1; L<=3; L++){
-					Xt = x+X;Yt = y+Y;
+					Xt = x+X*Xd;Yt = y+Y*Yd;
 					burntime = PixelList.GetMat(Xt, Yt, map, L).burnable;
 					if((int)(Math.random()*100+1)<=burntime){
 						map.setID(Xt, Yt, L, 32);
