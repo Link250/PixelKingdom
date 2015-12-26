975f78285d2ab36945fd0dfee3b5b4bf95f1e204
ultrasn0w <r.barth@dshack.org>
2015-12-26 20:10:54 +0100

    updated Launcher

--- windows_launcher/run.bat
+++ windows_launcher/run.bat
@@ -1,11 +1,11 @@
 @echo off
 
 echo ==========================================================
-echo                      PIXEL KINGDOM         [pub-Preview_5] 
+echo                      PIXEL KINGDOM            [PK-alpha_1]
 echo ==========================================================
 
 echo Starte PixelKingdom...
-java -jar PixelKingdom.jar -XX:+UseFastAccessorMethods -XX:+BindGCTaskThreadsToCPUs -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy
+java -jar PixelKingdom.jar -XX:+UseFastAccessorMethods -XX:+BindGCTaskThreadsToCPUs -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:-UseAdaptiveSizePolicy
 echo.
 echo.
 echo Errors bitte an Entwickler weitergeben mit Informationen zur Ursache /Eine Issue auf unserem GitHub erstellen.
