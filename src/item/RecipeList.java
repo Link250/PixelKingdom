a8c0b468c92f8a16a82f478c6486843ee3f887d7
QuantumHero <dani.bg97@web.de>
2016-10-27 23:37:31 +0200

    some "small" Ideas

--- src/item/RecipeList.java
+++ src/item/RecipeList.java
@@ -1,6 +1,7 @@
 package item;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 import item.Recipe.component;
 import main.Game;
@@ -8,24 +9,17 @@ import main.Game;
 public class RecipeList {
 
 	public ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+	public Set<Set<Recipe>> categories;
 	
 	public RecipeList(){
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(4, 10).addP(6, 30);
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(3, 10).addP(2, 10);
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(1, 50).addE(7, 20).addP(64, 20);
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(4, 10).addP(16, 1);
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(16, 5).addE(6, 10).addP(48, 5);
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(1, 30).addE(6, 10).addP(301, 1);
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(33, 30).addE(6, 10).addP(302, 1);
-		recipes.add(0, new Recipe());
-			recipes.get(0).addE(16, 1).addE(6, 3).addP(400, 5);
+		recipes.add(new Recipe().addE(4, 10).addP(6, 30));
+		recipes.add(new Recipe().addE(3, 10).addP(2, 10));
+		recipes.add(new Recipe().addE(1, 50).addE(7, 20).addP(64, 20));
+		recipes.add(new Recipe().addE(4, 10).addP(16, 1));
+		recipes.add(new Recipe().addE(16, 5).addE(6, 10).addP(48, 5));
+		recipes.add(new Recipe().addE(1, 30).addE(6, 10).addP(301, 1));
+		recipes.add(new Recipe().addE(33, 30).addE(6, 10).addP(302, 1));
+		recipes.add(new Recipe().addE(16, 1).addE(6, 3).addP(400, 5));
 		for(Recipe r : recipes){
 			String s = "Item(s) ";
 			for(component c : r.educts){s += "["+ItemList.GetItem(c.ID).name+" x "+c.n+"] ";}
